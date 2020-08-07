5a2cf8a39a9591ab2e299cd5cedb78660ada3d3a
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-07 23:15:41 +0900

    refactor: 인터페이스 도입

--- src/springbook/user/dao/DConnectionMaker.java
+++ src/springbook/user/dao/DConnectionMaker.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+public class DConnectionMaker implements ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8&serverTimezone=UTC", "root", "");
         return c;
