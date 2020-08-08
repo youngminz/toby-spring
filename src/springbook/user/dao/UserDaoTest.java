7ff0b2492183f5dd654a00a1dd14b751b3fd54f9
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-08 11:54:42 +0900

    refactor: 객체 생성을 DaoFactory로 이동

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -6,9 +6,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("whiteship");
