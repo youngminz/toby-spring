f03f82e1f4b6419d2752fb9ea221f6290e79e740
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-08 21:50:42 +0900

    refactor: 애플리케이션 컨텍스트 설정 파일을 Java에서 XML로 변경

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,14 +1,14 @@
 package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
