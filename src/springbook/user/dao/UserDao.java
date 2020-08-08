222da84e268e8e645f9d24739dd984f9fbf6d466
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-09 00:01:44 +0900

    refactor: 안 쓰는 예외 제거

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -29,7 +29,7 @@ public class UserDao {
         c.close();
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
+    public User get(String id) throws SQLException {
         Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
