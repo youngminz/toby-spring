6407da5664db85d03a8792ba2d712e632909f0ad
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-09 21:06:56 +0900

    refactor: 템플릿/콜백 패턴 적용

--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -6,16 +6,26 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
+        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer sum = 0;
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sum += Integer.valueOf(line);
+                }
+                return sum;
+            }
+        };
+        return fileReadTemplate(filepath, sumCallback);
+    }
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
         BufferedReader br = null;
 
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer sum = 0;
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sum += Integer.valueOf(line);
-            }
-            return sum;
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
