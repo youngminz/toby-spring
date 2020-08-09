88abb2bf3823e008cb920d5b92d5c6650c5d87f4
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-09 20:54:35 +0900

    feat: 예외 처리 추가

--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -6,14 +6,27 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filepath));
-        Integer sum = 0;
-        String line = null;
-        while ((line = br.readLine()) != null) {
-            sum += Integer.valueOf(line);
-        }
+        BufferedReader br = null;
 
-        br.close();
-        return sum;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer sum = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
