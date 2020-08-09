230790dbc928ffea6eb8672104e14478cde3a189
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-10 00:31:21 +0900

    feat: DuplicateKeyException -> DuplicateUserIdException으로 래핑

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,5 +1,6 @@
 package springbook.user.dao;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
@@ -28,10 +29,14 @@ public class UserDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(final User user) {
-        jdbcTemplate.update(
-                "insert into users (id, name, password) values (?, ?, ?)",
-                user.getId(), user.getName(), user.getPassword());
+    public void add(final User user) throws DuplicateKeyException {
+        try {
+            jdbcTemplate.update(
+                    "insert into users (id, name, password) values (?, ?, ?)",
+                    user.getId(), user.getName(), user.getPassword());
+        } catch (DuplicateKeyException e) {
+            throw new DuplicateUserIdException(e);
+        }
     }
 
     public User get(String id) {
