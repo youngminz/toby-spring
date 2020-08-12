99601dac5c39fbec7eefba1fed70b1131610cfac
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-13 02:12:09 +0900

    feat: 등급 업그레이드 시 메일 전송 기능 구현

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -1,5 +1,7 @@
 package springbook.user.service;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -64,5 +66,19 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEMail(user);
+    }
+
+    private void sendUpgradeEMail(User user) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("mail.server.com");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("useradmin@ksug.org");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다");
+
+        mailSender.send(mailMessage);
     }
 }
