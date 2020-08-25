9700d0bdf2db6d8ae48c5cb81fcd40866e3c4eb5
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-26 03:43:47 +0900

    refactor: Uppercase 부가기능을 InvocationHandler로 구현

--- src/springbook/learningtest/proxy/ProxyTest.java
+++ src/springbook/learningtest/proxy/ProxyTest.java
@@ -2,6 +2,8 @@ package springbook.learningtest.proxy;
 
 import org.junit.Test;
 
+import java.lang.reflect.Proxy;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -13,7 +15,11 @@ public class ProxyTest {
         assertThat(hello.sayHi("Toby"), is("Hi Toby"));
         assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
 
-        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Hello.class},
+                new UppercaseHandler(new HelloTarget())
+        );
         assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
         assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
         assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
