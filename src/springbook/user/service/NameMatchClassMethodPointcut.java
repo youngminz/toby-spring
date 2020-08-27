387126ff24996d2319db5a96c2d9213eefefab8e
Youngmin Koo <youngminz.kr@gmail.com>
2020-08-28 03:17:36 +0900

    refactor: 클래스 기반으로 포인트컷 대상을 지정하고 자동 프록시 생성기를 사용

---
+++ src/springbook/user/service/NameMatchClassMethodPointcut.java
@@ -0,0 +1,24 @@
+package springbook.user.service;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.util.PatternMatchUtils;
+
+public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut {
+    public void setMappedClassName(String mappedClassName) {
+        this.setClassFilter(new SimpleClassFilter(mappedClassName));
+    }
+
+    static class SimpleClassFilter implements ClassFilter {
+        String mappedName;
+
+        private SimpleClassFilter(String mappedName) {
+            this.mappedName = mappedName;
+        }
+
+        @Override
+        public boolean matches(Class<?> aClass) {
+            return PatternMatchUtils.simpleMatch(mappedName, aClass.getSimpleName());
+        }
+    }
+}
